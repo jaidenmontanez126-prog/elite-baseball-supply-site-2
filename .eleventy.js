44204640e8afa477b736dda2be1f950fa97cf37c
jaidenmontanez126-prog <272930610+jaidenmontanez126-prog@users.noreply.github.com>
2026-07-26 16:26:02 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("script.js");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("logo.png");
     eleventyConfig.addPassthroughCopy("_redirects");
+
 
     eleventyConfig.addCollection("posts", function(collectionApi) {
         return collectionApi.getFilteredByGlob("pages/blog/*.md");

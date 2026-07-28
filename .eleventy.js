46a5fe825a23b3430943f619c2a400ecd4687c64
jaidenmontanez126-prog <272930610+jaidenmontanez126-prog@users.noreply.github.com>
2026-07-28 16:27:09 -0400

    Complete website updates, navigation fixes, styling improvements, and blog setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,21 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("script.js");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("logo.png");
     eleventyConfig.addPassthroughCopy("_redirects");
-
-
-    eleventyConfig.addCollection("posts", function(collectionApi) {
-        return collectionApi.getFilteredByGlob("pages/blog/*.md");
-      });
+  
+    eleventyConfig.addCollection("posts", function (collectionApi) {
+      return collectionApi.getFilteredByGlob("pages/blog/*.md");
+    });
   
     return {
       dir: {
         input: ".",
         output: "_site"
-      }
+      },
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk"
     };
   };

50763d580c19f02d35abbf4793d9085bd733c708
jaidenmontanez126-prog <272930610+jaidenmontanez126-prog@users.noreply.github.com>
2026-09-19 21:01:03 -0400

    Spacing

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
+const MarkdownIt = require("markdown-it");
+function preserveBlankLines(markdown) {
+  return markdown.replace(/\n{3,}/g, (match) => {
+    const extraBlankLines = match.length - 2;
+    const spacers = '<div class="blank-line"></div>\n'.repeat(extraBlankLines);
+    return `\n\n${spacers}\n`;
+  });
+}
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("script.js");
